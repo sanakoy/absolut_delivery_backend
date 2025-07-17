1120580abd40cb4b66044d920f606e948ae1117f
sanakoy <boeiksanakoev@bk.ru>
2025-07-17 15:35:53 +0300

    fixed cors_allowed_origins v3

--- delivery/settings.py
+++ delivery/settings.py
@@ -168,7 +168,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOWED_ORIGINS = [
-    "https://absolutdeliverybackend-production.up.railway.app/"
+    "https://absolutdeliverybackend-production.up.railway.app/",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
